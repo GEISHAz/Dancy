1574dbbe8856e83a400113511d6f271f9b3961ee
남동우 <ndw8200@naver.com>
2024-01-28 22:10:52 +0900

    [SETTING] 백앤드 서버 파트 초기 세팅

---
+++ server/src/main/java/com/ssafy/dancy/DancyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.dancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DancyApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/ssafy/dancy/DancyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.dancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DancyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
