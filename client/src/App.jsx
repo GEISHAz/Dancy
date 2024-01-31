26aedfd10a89b5d068c9da962fca4827f02f7bb9
박설연 <s303y@naver.com>
2024-01-31 16:39:09 +0900

    [FEAT] 글로벌 스타일 설정
    Change-Id: I526ac056011ad46b01cd9ede6cfa1f7362598ef3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,27 @@
-import { BrowserRouter as RouterProvider } from 'react-router-dom';
-import NavigationBar from './components/NavigationBar/NavigationBar';
-import Router from './components/Router'
+import { BrowserRouter as RouterProvider } from "react-router-dom";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
+import Router from "./components/Router";
+
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: #fff9f6; /* 원하는 배경색으로 설정 */
+  }
+
+  /* 다른 전역 스타일 추가 가능. */
+`;
 
 export default function App() {
-	return (
-		<RouterProvider>
-			<NavigationBar />
-			<Router />
-		</RouterProvider>
-	);
+  return (
+    <>
+      <GlobalStyle />
+      <RouterProvider>
+        <NavigationBar />
+        <Router />
+      </RouterProvider>
+    </>
+  );
 }
