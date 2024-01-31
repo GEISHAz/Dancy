c91359f82830a3d5423f0e771278f9bc874ff8ea
이해진 <sixb_0417@hanmail.net>
2024-01-31 14:53:52 +0900

    [FEAT] Profile 소개란 UI 개발
    Change-Id: I84986d81e3154976b165a700fb4f99c27a1a09ff

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,16 @@
+import ProfileIntroduct from "../components/Profile/ProfileIntroduct/ProfileIntroduct"
+
 export default function Profile() {
-    return (
-        <div>Profile Page</div>
-    )
+	const Profile = {
+		nickName: 'namhyun._.',
+		introduceText: `안 아픈 줄 알았는데......\n 너무 아파서..\n감각이 없었던 거 였어.`,
+		profileImageUrl: '',
+		following: 1,
+		follower: 672,
+		followed: true,
+	}
+	
+	return (
+		<ProfileIntroduct Profile={Profile} />
+	)
 }
