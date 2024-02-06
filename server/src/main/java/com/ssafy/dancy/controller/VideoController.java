28edcd17bba96ab3893ffcfd290436104a764996
조남현 <zetta_byte@naver.com>
2024-02-06 13:13:44 +0900

    [FEAT] Video Info test
    Change-Id: Ie5edf82addf99ebf29eba4f544d73c2974ed7b63

--- server/src/main/java/com/ssafy/dancy/controller/VideoController.java
+++ server/src/main/java/com/ssafy/dancy/controller/VideoController.java
@@ -10,9 +10,11 @@ import com.ssafy.dancy.service.video.VideoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
@@ -24,11 +26,29 @@ import java.util.List;
 public class VideoController {
 
     private final VideoService videoService;
+    WebClient webClient = WebClient.create();
 
     @PostMapping("")
-    public String sendAccuracyData(@RequestBody String accuracyData){
+    public String requestAccuracyData(@RequestBody String accuracyData){
 
-        return accuracyData;
+
+        try{
+            String apiUrl = "http://i10d210.p.ssafy.io:5000/sendData";
+
+            String responseBody = webClient.post()
+                    .uri(apiUrl)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .bodyValue(new String("Spring에서 저장한 url"))
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+            return responseBody;
+
+        }
+        catch(Exception e){
+            return "ERROR";
+        }
+
     }
 
 }
