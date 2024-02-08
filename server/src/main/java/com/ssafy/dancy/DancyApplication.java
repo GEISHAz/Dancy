0af9042e5c073de924d1c4f672006a6fa74c3131
남동우 <ndw8200@naver.com>
2024-02-08 11:14:53 +0900

    [FIX] 서버 시간이 UTC(영국 시간) 으로 되어 있는 부분 수정 시도
    Change-Id: Ie2aa6981a78a0bebec1d15595addd71d369d08d2

--- server/src/main/java/com/ssafy/dancy/DancyApplication.java
+++ server/src/main/java/com/ssafy/dancy/DancyApplication.java
@@ -1,10 +1,13 @@
 package com.ssafy.dancy;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.TimeZone;
 
 @EnableEncryptableProperties
 @SpringBootApplication
@@ -14,6 +17,11 @@ public class DancyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DancyApplication.class, args);
+	}
+
+	@PostConstruct
+	void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
