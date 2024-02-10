b1baf93cae582ecac514a3e5e69f037fc1516ba5
박설연 <s303y@naver.com>
2024-02-10 20:00:18 +0900

    [ADD] 로그아웃을 누르면 userState의 값을 초기화해주도록 변경
    Change-Id: I344b6108975a1eca1e678554991bb9de972b6849

--- client/src/components/NavigationBar/NavigationBar.jsx
+++ client/src/components/NavigationBar/NavigationBar.jsx
@@ -1,16 +1,15 @@
-import { useState, useEffect } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import SearchBar from './SearchBar';
-import Notification from './Notification';
-import * as N from './NavigationBar.style';
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import SearchBar from "./SearchBar";
+import Notification from "./Notification";
+import * as N from "./NavigationBar.style";
 import { loginState, userState } from "../../recoil/LoginState.js";
-import { useRecoilValue, useRecoilState, useSetRecoilState } from 'recoil';
-import { logout } from '../../api/auth.js';
-import { userDetails } from '../../api/user.js';
-
+import { useRecoilValue, useRecoilState, useSetRecoilState } from "recoil";
+import { logout } from "../../api/auth.js";
+import { userDetails } from "../../api/user.js";
 
 export default function Navbar() {
-  const [activeButton, setActiveButton] = useState('');
+  const [activeButton, setActiveButton] = useState("");
   const [userInfo, setUserInfo] = useRecoilState(userState);
   const [isLoggedIn, setLoginState] = useRecoilState(loginState);
   const userDetailsInfo = useRecoilValue(userState);
@@ -19,47 +18,67 @@ export default function Navbar() {
 
   const logoutHandler = () => {
     logout(setLoginState)
-    .then((res) => {
-        navigate('/')
-    })
-    .catch((err) => console.error(err));
-  }
+      .then((res) => {
+        // Recoil 상태 초기화
+        setUserInfo({
+          email: "",
+          nickname: "",
+          birthDate: "",
+          introduceText: "",
+          gender: "",
+          profileImageUrl: null,
+        });
+        navigate("/");
+      })
+      .catch((err) => console.error(err));
+  };
 
   return (
     <N.NavArea>
       <N.NavRed />
       <N.NavTextArea>
         <N.NavLeft>
-        <N.NavLeftContainer>
-        <Link to="/">
-          <N.NavHome onClick={() => setActiveButton('Home')} $active={activeButton === 'Home'}>
-            Home
-          </N.NavHome>
-        </Link>
-        <N.Square />
-        </N.NavLeftContainer>
-        <N.NavLeftContainer>
-          <Link to="/create">
-            <N.NavPractice onClick={() => setActiveButton('Create')} $active={activeButton === 'Create'}>
-              Practice
-            </N.NavPractice>
-          </Link>
-          <N.Square />
-        </N.NavLeftContainer>
-        <N.NavLeftContainer>
-          <Link to="/stage">
-            <N.NavStage onClick={() => setActiveButton('Stage')} $active={activeButton === 'Stage'}>
-              Stage
-            </N.NavStage>
+          <N.NavLeftContainer>
+            <Link to="/">
+              <N.NavHome onClick={() => setActiveButton("Home")} $active={activeButton === "Home"}>
+                Home
+              </N.NavHome>
+            </Link>
             <N.Square />
-          </Link>
-        </N.NavLeftContainer>
-        <N.NavLeftContainer>
-          <Link to={`/profile/${userDetailsInfo.nickname}`}>
-              <N.NavProfile onClick={() => setActiveButton('Profile')} $active={activeButton === 'Profile'}>Profile</N.NavProfile>
-          </Link>
-          <N.Square />
-        </N.NavLeftContainer>
+          </N.NavLeftContainer>
+          <N.NavLeftContainer>
+            <Link to="/create">
+              <N.NavPractice
+                onClick={() => setActiveButton("Create")}
+                $active={activeButton === "Create"}
+              >
+                Practice
+              </N.NavPractice>
+            </Link>
+            <N.Square />
+          </N.NavLeftContainer>
+          <N.NavLeftContainer>
+            <Link to="/stage">
+              <N.NavStage
+                onClick={() => setActiveButton("Stage")}
+                $active={activeButton === "Stage"}
+              >
+                Stage
+              </N.NavStage>
+              <N.Square />
+            </Link>
+          </N.NavLeftContainer>
+          <N.NavLeftContainer>
+            <Link to={`/profile/${userDetailsInfo.nickname}`}>
+              <N.NavProfile
+                onClick={() => setActiveButton("Profile")}
+                $active={activeButton === "Profile"}
+              >
+                Profile
+              </N.NavProfile>
+            </Link>
+            <N.Square />
+          </N.NavLeftContainer>
         </N.NavLeft>
         <N.NavRight>
           <SearchBar />
@@ -67,28 +86,28 @@ export default function Navbar() {
             <Notification />
           </N.AlertButton>
           <N.NavLogin>
-          {isLogin ? (
-            <N.NavLoginWrapper>
-              <Link to='/setting'>
-                <N.NavUserProfileImage src={userDetailsInfo.profileImageUrl}/>
-              </Link>
-              <N.NavProfileArea>
-                <Link to='/setting'>
-                  <N.NavUserName>{userDetailsInfo?.nickname} 님</N.NavUserName>
+            {isLogin ? (
+              <N.NavLoginWrapper>
+                <Link to="/setting">
+                  <N.NavUserProfileImage src={userDetailsInfo.profileImageUrl} />
                 </Link>
-              <N.NavLogout onClick={logoutHandler}>Logout</N.NavLogout>
-              </N.NavProfileArea>
-            </N.NavLoginWrapper>
-          ) : (
-            <N.NavLogoutWrapper>
-              <N.NavSignUp>
-                <Link to="/signup">Join</Link>
-              </N.NavSignUp>
-              <N.NavLogin>
-                <Link to="/login">Login</Link>
-              </N.NavLogin>
-            </N.NavLogoutWrapper>
-          )}
+                <N.NavProfileArea>
+                  <Link to="/setting">
+                    <N.NavUserName>{userDetailsInfo?.nickname} 님</N.NavUserName>
+                  </Link>
+                  <N.NavLogout onClick={logoutHandler}>Logout</N.NavLogout>
+                </N.NavProfileArea>
+              </N.NavLoginWrapper>
+            ) : (
+              <N.NavLogoutWrapper>
+                <N.NavSignUp>
+                  <Link to="/signup">Join</Link>
+                </N.NavSignUp>
+                <N.NavLogin>
+                  <Link to="/login">Login</Link>
+                </N.NavLogin>
+              </N.NavLogoutWrapper>
+            )}
           </N.NavLogin>
         </N.NavRight>
       </N.NavTextArea>
