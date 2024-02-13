ecc052e323dce36171cc43971157237a0ab8d129
강정수 <bbbbqz@nate.com>
2024-02-13 14:10:16 +0900

    [FIX] 팔로워 클릭 시 해당 프로필로 이동 완.
    Change-Id: Ib3a2bedb4ae12916568ed89471609285961b4a94

--- client/src/components/Profile/ProfileIntroduct/FollowItem.jsx
+++ client/src/components/Profile/ProfileIntroduct/FollowItem.jsx
@@ -1,10 +1,21 @@
 import * as F from './FollowItem.style'
+import { useNavigate } from 'react-router-dom';
 
-export const FollowItem = ({ profileImageUrl, nickname, following, key }) => {
+export const FollowItem = ({ profileImageUrl, nickname, following, key, getData }) => {
+  const data = false;
+  const navigate = useNavigate();
+
+	const postData = () => {
+		getData(data)
+    navigate(`/profile/${nickname}`)
+    window.location.reload();
+	}
   console.log(profileImageUrl, nickname, following, key)
+
+
   return (
     <F.Profile>
-      <div className='flex items-center gap-x-2'>
+      <div className='flex items-center gap-x-2' onClick={postData} >
         <F.ProfileImg src={profileImageUrl}/>
         <F.NickName>{nickname}</F.NickName>
       </div>
