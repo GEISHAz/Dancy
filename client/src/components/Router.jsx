fe30010d29d07e7bafa5927b6da9ddf3afd5d534
박설연 <s303y@naver.com>
2024-02-15 14:35:48 +0900

    [FIX] 회원가입 관련 라우터 조정
    Change-Id: I1d6590292a490e96e547ec3b963723a8b2845630

--- client/src/components/Router.jsx
+++ client/src/components/Router.jsx
@@ -36,7 +36,7 @@ export default function Router({ cardDetails, videoDetails }) {
       location.pathname !== "/" &&
       location.pathname !== "/login" &&
       location.pathname !== "/signup" &&
-      location.pathname !== "/signup/joinform" &&
+      // location.pathname !== "/signup/joinform" &&
       location.pathname !== "/signup/joincomplete" &&
       location.pathname !== "/findpassword"
     ) {
@@ -117,8 +117,8 @@ export default function Router({ cardDetails, videoDetails }) {
           <>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/signup/joinform" element={<JoinForm />} />
+            <Route path="/signup" element={<JoinForm />} />
+            {/* <Route path="/signup/joinform" element={<JoinForm />} /> */}
             <Route path="/signup/joincomplete" element={<JoinComplete />} />
             <Route path="/findpassword" element={<SendPin />} />
             {/* <Route path="*" element={<Login />} /> */}
