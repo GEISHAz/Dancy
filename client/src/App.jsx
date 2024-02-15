9bf789f54a505a06e883b00bac458104b71a85a9
박설연 <s303y@naver.com>
2024-02-15 14:25:24 +0900

    [FIX] 글씨체 2차
    Change-Id: Ibdc625fd9d40e7d7e7568fd02928344e0eac7815

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,56 +5,68 @@ import Router from "./components/Router";
 import Footer from "./components/Footer/Footer";
 import GoCreate from "./components/Footer/GoCreate";
 
+import bmJUATTF from "./fonts/BM JUA_TTF.ttf";
+import nsB from "./fonts/NanumSquareRoundB.ttf";
+import nsEB from "./fonts/NanumSquareRoundEB.ttf";
+import nsL from "./fonts/NanumSquareRoundL.ttf";
+import nsR from "./fonts/NanumSquareRoundR.ttf";
+import psr from "./fonts/PartialSansKR-Regular.otf";
+import nyjB from "./fonts/남양주 고딕 B.otf";
+import nyjEB from "./fonts/남양주 고딕 EB.otf";
+import nyjL from "./fonts/남양주 고딕 L.otf";
+
+
+
 export const GlobalStyle = createGlobalStyle`
   @font-face {
     font-family: 'BM JUA_TTF';
-    src: url('./fonts/BM JUA_TTF.ttf') format('truetype');
+    src: url(${bmJUATTF}) format('truetype');
   }
 
   @font-face {
     font-family: 'NanumSquareRound';
-    src: url('./fonts/NanumSquareRoundB.ttf') format('truetype');
+    src: url(${nsB}) format('truetype');
     font-weight: bold;
   }
 
   @font-face {
     font-family: 'NanumSquareRound';
-    src: url('./fonts/NanumSquareRoundEB.ttf') format('truetype');
+    src: url(${nsEB}) format('truetype');
     font-weight: 800; // Extra Bold
   }
 
   @font-face {
     font-family: 'NanumSquareRound';
-    src: url('./fonts/NanumSquareRoundL.ttf') format('truetype');
+    src: url(${nsL}) format('truetype');
     font-weight: 300; // Light
   }
 
   @font-face {
     font-family: 'NanumSquareRound';
-    src: url('./fonts/NanumSquareRoundR.ttf') format('truetype');
+    src: url(${nsR}) format('truetype');
     font-weight: normal;
   }
 
   @font-face {
     font-family: 'PartialSansKR';
-    src: url('./fonts/PartialSansKR-Regular.otf') format('opentype');
+    src: url(${psr}) format('opentype');
   }
 
   @font-face {
     font-family: 'NYJ Gothic B';
-    src: url('./fonts/남양주 고딕 B.otf') format('opentype');
+    src: url(${nyjB}) format('opentype');
     font-weight: bold;
   }
 
   @font-face {
     font-family: 'NYJ Gothic EB';
-    src: url('./fonts/남양주 고딕 EB.otf') format('opentype');
+    src: url(${nyjEB}) format('opentype');
     font-weight: 800; // Extra Bold
   }
 
   @font-face {
     font-family: 'NYJ Gothic L';
-    src: url('./fonts/남양주 고딕 L.otf') format('opentype');
+    src: url(${nyjL}) format('opentype');
     font-weight: 300; // Light
   }
   body {
